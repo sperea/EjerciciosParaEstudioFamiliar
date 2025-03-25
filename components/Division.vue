f62218772ccd3e33bba8d41ccc695063e6ea6ee7
Sergio Perea <sperea@protonmail.com>
2025-03-25 17:42:32 +0100

    divisiones

--- components/Division.vue
+++ components/Division.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex items-center justify-center my-4">
-    <div class="text-2xl font-bold  pr-4">{{ dividend }}</div>
-    <div class="flex flex-col items-center">
-
+    <div class="flex items-center justify-center my-4">
+      <div class="text-2xl font-bold pr-4">{{ dividend }}</div>
       <div class="border-l-4 border-b-4 border-gray-800 h-12 w-12 flex items-center justify-center">
-        <div class="text-xl ">{{ divisor }}</div>
+        <div class="text-xl">{{ divisor }}</div>
       </div>
+    <div class="mt-2">
+      <!-- Removed quotient and remainder display -->
     </div>
   </div>
 </template>
@@ -17,6 +17,10 @@ const props = defineProps({
   dividend: Number,
   divisor: Number
 });
+
+// Calculate quotient and remainder
+const quotient = Math.floor(props.dividend / props.divisor);
+const remainder = props.dividend % props.divisor;
 </script>
 
 <style scoped>
